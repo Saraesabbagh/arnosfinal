7c730e2ea25b0db118491e3b9e476d85b3c4acbc
saraesabbagh <sarasabbagh@hotmail.co.uk>
2023-11-29 13:44:51 +0000

    changed events posters to include xmas ones

--- src/components/pages/Eventspage.js
+++ src/components/pages/Eventspage.js
@@ -13,18 +13,28 @@ export const Eventspage = () => {
           </div>
         </div>
       </section>
-      <section class="events-poster">
-        <img
-          src={require("../images/halloweenposter.jpg")}
-          alt="park events list"
-          class="img-fluid row mt-3 mb-4 "
-        />
-        <img
-          src={require("../images/parkevents.jpeg")}
-          alt="park events list"
-          class="img-fluid row mt-3 mb-4 "
-        />
-      </section>
+      <div class="center-container">
+        <section class="events-poster">
+          <img
+            src={require("../images/xmaseventposter.jpeg")}
+            alt="park events list"
+            class="img-fluid row mt-3 mb-4 "
+            style={{ width: "500px", height: "auto" }}
+          />
+          <img
+            src={require("../images/XmasParkFunrun.jpg")}
+            alt="park events list"
+            class="img-fluid row mt-3 mb-4 "
+            style={{ width: "500px", height: "auto" }}
+          />
+          <img
+            src={require("../images/parkevents.jpeg")}
+            alt="park events list"
+            class="img-fluid row mt-3 mb-4 "
+            style={{ width: "500px", height: "auto" }}
+          />
+        </section>
+      </div>
     </div>
   );
 };
